49ab77c6c1294e24e3b66d8ecd52590129efda35
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-04-01 12:04:54 +0530

    updated theme prefrence lock

--- src/utlis/Theme.jsx
+++ src/utlis/Theme.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Style from './Theme.module.css'
+import Cookies from 'js-cookie'
+
 const Theme = () => {
     const setDarkMode = () => {
-        document.querySelector("body").setAttribute('data-theme', 'dark')
+        document.querySelector("body").setAttribute('data-theme', 'dark');
+        Cookies.set('theme', 'dark', { expires: 1 }); // expires after 1 day
     }
     const setLightMode = () => {
-        document.querySelector("body").setAttribute('data-theme', 'light')
+        document.querySelector("body").setAttribute('data-theme', 'light');
+        Cookies.set('theme', 'light', { expires: 1 }); // expires after 1 day
     }
     const toggleTheme = (e) => {
         if (e.target.checked) setDarkMode();
         else setLightMode();
     }
+
+    useEffect(() => {
+        const theme = Cookies.get('theme');
+        if (theme === 'dark') {
+            setDarkMode();
+            document.querySelector('.toggle-switch').checked = true;
+        } else {
+            setLightMode();
+            document.querySelector('.toggle-switch').checked = false;
+        }
+    }, []);
+
     return (
         <>
             <label className={Style.switch}>
-                <input type="checkbox" className={Style.checkbox} onChange={toggleTheme} />
+                <input type="checkbox" className={Style.checkbox + ' toggle-switch'} onChange={toggleTheme} />
                 <div className={Style.slider}></div>
             </label>
         </>
