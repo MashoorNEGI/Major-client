9b457c48a8e42a395bbe4b045562ef5738e8568c
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-04-01 20:01:11 +0530

    css styling

---
+++ src/Pages/Schedule.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Outlet, NavLink } from 'react-router-dom'
+import {
+  FaAngleRight,
+  FaAngleLeft,
+  FaShoppingCart,
+  FaCog,
+  FaSignOutAlt,
+  FaBars
+} from 'react-icons/fa';
+import { BiSearchAlt } from 'react-icons/bi'
+import { MdFavoriteBorder } from 'react-icons/md'
+import Style from './css/sidebar.module.css'
+const ICON_SIZE = 20;
+
+const Schedule = ({ visible, show }) => {
+  return (
+    <>
+      <div className={Style.mobilenav}>
+        <button
+          className={Style.mobilenavbtn}
+          onClick={() => show(!visible)}
+        >
+          <FaBars size={24} />
+        </button>
+      </div>
+      <nav className={!visible ? Style.navbar : Style.sidebar}>
+        <button
+          type="button"
+          className={Style.navbtn}
+          onClick={() => show(!visible)}
+        >
+          {!visible
+            ? <FaAngleRight size={30} /> : <FaAngleLeft size={30} />}
+        </button>
+        <div className={Style.navwidth}>
+          <div className={Style.navwidth}>
+            <NavLink to="/dashboard" className={Style.navlink}>
+              <MdFavoriteBorder size={ICON_SIZE} />
+              <span>Favourite</span>
+            </NavLink>
+            <NavLink to="/analytics" className={Style.navlink}>
+              <BiSearchAlt size={ICON_SIZE} />
+              <span>Search</span>
+            </NavLink>
+            <NavLink to="/orders" className={Style.navlink}>
+              <FaShoppingCart size={ICON_SIZE} />
+              <span>Orders</span>
+            </NavLink>
+          </div>
+        </div>
+
+        <div className={Style.navwidth}>
+          <NavLink to="/settings" className={Style.navlink}>
+            <FaCog size={ICON_SIZE} />
+            <span>Settings</span>
+          </NavLink>
+          <NavLink to="/Sign-out" className={Style.navlink}>
+            <FaSignOutAlt size={ICON_SIZE} />
+            <span>Logout</span>
+          </NavLink>
+        </div>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+}
+
+export default Schedule
--- src/services/URL.js
+++ src/services/URL.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "http://localhost:3000"
-const BASE_URL = "https://wild-pear-dugong-cape.cyclic.app"
+const BASE_URL = "http://localhost:3000"
+// const BASE_URL = "https://wild-pear-dugong-cape.cyclic.app"
 
 export default  BASE_URL
