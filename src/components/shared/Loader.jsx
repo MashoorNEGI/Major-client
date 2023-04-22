347ddb41751aad3b9f1fb7815d1b39417edb605e
MashoorNEGI <ayushdeepnegi@gmail.com>
2023-04-22 10:37:15 +0530

    updated api loader

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -9,22 +9,18 @@ export const Loader = () => {
             <span></span>
             <span></span>
         </div>
-  )
+    )
 }
 
 export const APIloader = () => {
     return (
-        <div className={Style.API}>
-            <div className={Style.spinner}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+        <div className={Style.center}>
+            <svg className={Style.pl} width="240" height="240" viewBox="0 0 240 240">
+                <circle className={`${Style.pl__ring} ${Style.pl__ringa}`} cx="120" cy="120" r="105" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 660" stroke-dashoffset="-330" stroke-linecap="round"></circle>
+                <circle className={`${Style.pl__ring} ${Style.pl__ringb}`} cx="120" cy="120" r="35" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 220" stroke-dashoffset="-110" stroke-linecap="round"></circle>
+                <circle className={`${Style.pl__ring} ${Style.pl__ringc}`} cx="85" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+                <circle className={`${Style.pl__ring} ${Style.pl__ringd}`} cx="155" cy="120" r="70" fill="none" stroke="#000" stroke-width="20" stroke-dasharray="0 440" stroke-linecap="round"></circle>
+            </svg >
         </div>
     )
 }
